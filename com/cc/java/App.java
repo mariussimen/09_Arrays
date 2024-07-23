71e08656df4dfef729fcd6ec8490ffb01014f79f
mariussimen <theodores03@yahoo.fr>
2024-07-23 09:40:08 +0200

    Arrays

--- com/cc/java/App.java
+++ com/cc/java/App.java
@@ -7,86 +7,86 @@ public class App {
     public static void main(String[] args) {
 
       	// // Deklaration: int
-		// int [] intArr = new int [4];
-		// output("Länge Array: " + intArr.length);	
+		int [] intArr = new int [4];
+		output("Länge Array: " + intArr.length);	
 		
 		// // Index
-		// output("3. Element " + intArr[2]);
+		output("3. Element " + intArr[2]);
 		
-		// intArr[0] = 10;
-		// output("1. Element " + intArr[0]);
-		// intArr[intArr.length - 1] = 11;
+		intArr[0] = 10;
+		output("1. Element " + intArr[0]);
+		intArr[intArr.length - 1] = 11;
 		
-		// output("------------------ ");
+		output("------------------ ");
 
-		// for (int i = 0; i < intArr.length; i++) {
-		// 	output("Element: " + i + " : "+ intArr[i]);
-		// }
+		for (int i = 0; i < intArr.length; i++) {
+			output("Element: " + i + " : "+ intArr[i]);
+		}
 			
 		// Deklaration: String
 		// String [] strArr = new String [2];
-		// String [] strArr = {"Hallo","Nikola"};
+		String [] strArr = {"Hallo","Nikola"};
 		
-		// for (int i = 0; i < strArr.length; i++) {
-		// 	output("Element: " + strArr[i]);
-		// }
+		for (int i = 0; i < strArr.length; i++) {
+			output("Element: " + strArr[i]);
+		}
 		
 		
 		// Mehrdimensionale Arrays
 		// String [][] strMdArr = new String [2][2];
 		
-		// String [][] strMdArr = {{"Max","Mustermann"},{"Maxine","Musterfrau"}};
+		String [][] strMdArr = {{"Max","Mustermann"},{"Maxine","Musterfrau"}};
 		
 		// {{0,2,1},{1,0,0}}  x|y|z
 		
-		// output(strMdArr[0][0]);
-		// output(strMdArr[1][1]);
+		output(strMdArr[0][0]);
+		output(strMdArr[1][1]);
 		
 		// // nested loops
-		// for (int i = 0; i < strMdArr.length; i++) {
-		// 	for (int j = 0; j < strMdArr.length; j++) {
-		// 		output("Indizes i/j: " + "i:" +  i + " j:"+ j + " " + strMdArr[i][j]);
-		// 	}
-		// }
+		for (int i = 0; i < strMdArr.length; i++) {
+			for (int j = 0; j < strMdArr.length; j++) {
+				output("Indizes i/j: " + "i:" +  i + " j:"+ j + " " + strMdArr[i][j]);
+			}
+		}
 		
 		// // Deklaration | Chars
-		// char [] letters = {'d','c','a','A','b'};
+		char [] letters = {'d','c','a','A','b'};
 	
-		// 	output("");
-		// 	output("---- vor Sortierung: ");
-		// 	for (int i = 0; i < letters.length; i++) {
-		// 		output(letters[i]);	
-		// 	}
+			output("");
+			output("---- vor Sortierung: ");
+			for (int i = 0; i < letters.length; i++) {
+				output(letters[i]);	
+			}
 
 		// // Sortierfkt. --> sortiertes Array
-		// Arrays.sort(letters);  // Statische Klasse | Methode
+		Arrays.sort(letters);  // Statische Klasse | Methode
 
-		// output("");
-		// output("---- nach Sortierung: ");
-		// for (int i = 0; i < letters.length; i++) {
-		// 	output(letters[i]);	
-		// }
+		output("");
+		output("---- nach Sortierung: ");
+		for (int i = 0; i < letters.length; i++) {
+			output(letters[i]);	
+		}
 
-		// 	output("");
-		// 	output("---- Suche: erfolgreich ----");
-		// 	// ... danach Suche (1)
-		// 	int searchIndex = Arrays.binarySearch(letters,'a');
-		// 	output("");
-		// 	output("---- Binäre Suche: ");
-		// 	output("searchIndex: " + searchIndex);
+			output("");
+			output("---- Suche: erfolgreich ----");
+			// ... danach Suche (1)
+			int searchIndex = Arrays.binarySearch(letters,'a');
+			output("");
+			output("---- Binäre Suche: ");
+			output("searchIndex: " + searchIndex);
 		
 		// 	// output("---- Suche: erfolglos ----");
 		// 	// ... danach Suche (2)
-		// 	searchIndex = Arrays.binarySearch(letters,'e');
-		// 	output("searchIndex: " + searchIndex);  // -(length+1)
-		// 	// .... isInArray()
+			searchIndex = Arrays.binarySearch(letters,'e');
+			output("searchIndex: " + searchIndex);  // -(length+1)
+			// .... isInArray()
 			
-		// 	output("---- fill() ----");
-		// 	//.. Array füllen
-		// 	Arrays.fill(letters, '0');
-		// 	for (int i = 0; i < letters.length; i++) {
-		// 		output((letters[i]));
-		// 	}
+			output("---- fill() ----");
+			//.. Array füllen
+			Arrays.fill(letters, '0');
+			for (int i = 0; i < letters.length; i++) {
+				output((letters[i]));
+			}
 
     }
 
